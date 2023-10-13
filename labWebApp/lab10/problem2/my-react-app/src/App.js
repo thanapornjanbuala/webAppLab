61ae2094d34201544171d7f6d87befde0b6a792d
thanapornjanbuala <thanaporn.j@kkumail.com>
2023-10-13 21:42:27 +0700

    lab10

--- labWebApp/lab10/problem2/my-react-app/src/App.js
+++ labWebApp/lab10/problem2/my-react-app/src/App.js
@@ -1,14 +1,72 @@
-import React, { useState } from 'react';
-import PersonForm from './PersonForm';
-import UniversityForm from './UniversityForm';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [personName1, setPersonName1] = useState('');
+  const [personName2, setPersonName2] = useState('');
   const [universityName, setUniversityName] = useState('');
 
+  const handlePersonName1Change = (event) => {
+    setPersonName1(event.target.value);
+  };
+
+  const handlePersonName2Change = (event) => {
+    setPersonName2(event.target.value);
+  };
+
+  const handleUniversityNameChange = (event) => {
+    setUniversityName(event.target.value);
+  };
+
+  useEffect(() => {
+    if (personName2 === '') {
+      setPersonName2(personName1);
+    }
+  }, [personName1, personName2]);
+
   return (
-    <div className="App">
-      <PersonForm />
-      <UniversityForm universityName={universityName} />
+    <div>
+      <h1>Forms with Names</h1>
+      <div className="form-container">
+        <form>
+          <label htmlFor="personName1"></label>
+          <input
+            type="text"
+            id="personName1"
+            placeholder="Enter Name"
+            value={personName1}
+            onChange={handlePersonName1Change}
+          />
+          <label htmlFor="universityName"> Studies in </label>
+          <input
+            type="text"
+            placeholder="Enter University"
+            value={universityName}
+            onChange={handleUniversityNameChange}
+          />
+        </form>
+      </div>
+
+      <br />
+
+      <div className="form-container">
+        <form>
+          <label htmlFor="personName2"></label>
+          <input
+            type="text"
+            id="personName2"
+            placeholder="Enter Name"
+            value={personName2}
+            onChange={handlePersonName2Change}
+          />
+          <label htmlFor="universityName"> Studies in </label>
+          <input
+            type="text"
+            placeholder="Enter University"
+            value={universityName}
+            onChange={handleUniversityNameChange}
+          />
+        </form>
+      </div>
     </div>
   );
 }
